3ae9401347c11a6c1c3dc92b4e8e469c01b57b48
Shreyan Gupta <61325229+Xenon8778@users.noreply.github.com>
2025-11-26 10:57:48 -0500

    Fix imports and update DataFrame usage in tests

--- tests/test_scPII.py
+++ tests/test_scPII.py
@@ -1,13 +1,9 @@
-import networkx as nx
-import pandas as pd
-from scPII.core import scPRS
-
 def test_scPRS_no_cluster():
-    G = nx.powerlaw_cluster_graph(25, 1, 0.6, seed=0)
-    inputnet = pd.DataFrame(nx.adjacency_matrix(G).todense())
+    G = powerlaw_cluster_graph(25, 1, 0.6, seed=0)
+    inputnet = DataFrame(nx.adjacency_matrix(G).todense())
 
     PRSout = scPRS(inputnet, cluster = False, getPval=False)
 
     assert len(PRSout) == 3
-    assert isinstance(PRSout['PRSmatrix'], pd.DataFrame)
-    assert isinstance(PRSout['Summary'], pd.DataFrame)
+    assert isinstance(PRSout['PRSmatrix'], DataFrame)
+    assert isinstance(PRSout['Summary'], DataFrame)
