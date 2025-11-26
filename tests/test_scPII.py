dc07436e4d2384deed5ecd52e7731029dbfcf866
Shreyan Gupta <61325229+Xenon8778@users.noreply.github.com>
2025-11-26 11:08:48 -0500

    Add genes parameter to scPRS function call

--- tests/test_scPII.py
+++ tests/test_scPII.py
@@ -6,7 +6,7 @@ def test_scPRS_no_cluster():
     G = powerlaw_cluster_graph(25, 1, 0.6, seed=0)
     inputnet = DataFrame(adjacency_matrix(G).todense())
 
-    PRSout = scPRS(inputnet, cluster = False, getPval=False)
+    PRSout = scPRS(inputnet, genes=inputnet.index, cluster = False, getPval=False, n_comps=5)
 
     assert len(PRSout) == 3
     assert isinstance(PRSout['PRSmatrix'], DataFrame)
